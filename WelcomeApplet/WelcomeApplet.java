63e5cbada7713a6cdb8c235a0531b408e3f7dfba
wuqiangroy <wuqiangroy@live.com>
2017-11-08 22:40:00 +0800

    touch more java code

---
+++ WelcomeApplet/WelcomeApplet.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import javax.swing.*;
+
+
+public class WelcomeApplet extends JApplet {
+    public void init() {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                setLayout(new BorderLayout());
+                JLabel label = new JLabel(getParameter("greeting"), SwingConstants.CENTER);
+                label.setFont(new Font("Serif", Font.BOLD, 18));
+                add(label, BorderLayout.CENTER);
+
+                JPanel panel = new JPanel();
+
+                JButton cayButton = new JButton("Cay Horstmann");
+                cayButton.addActionListener(makeAction("http://www.horstmann.com"));
+                panel.add(cayButton);
+
+                JButton grayButton = new JButton("Gray Cornell");
+                grayButton.addActionListener(makeAction("mailto:wuqiangroy@gmail.com"));
+                panel.add(grayButton);
+
+                add(panel, BorderLayout.SOUTH);
+            }
+        });
+    }
+
+    private ActionListener makeAction(final String urlString) {
+        return new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                try {
+                    getAppletContext().showDocument(new URL(urlString));
+                }
+                catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+}
