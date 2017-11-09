a413f81a0e46448b905d67ac222417fe697a65eb
wuqiangroy <wuqiangroy@live.com>
2017-11-10 00:24:13 +0800

    while

---
+++ CJ3/Retirement/Retirement.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Retirement {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How much money do you need to retire?");
+        double goal = in.nextDouble();
+
+        System.out.println("How much money will you contribute every year?");
+        double payment = in.nextDouble();
+
+        System.out.println("Interest rate in %: ");
+        double interestRate = in.nextDouble();
+
+        double balance = 0;
+        int year = 0;
+
+        while (balance < goal) {
+            balance += payment;
+            double interest = balance * interestRate / 100;
+            balance += interest;
+            year++;
+        }
+        System.out.printf("you can retire in %d years.", year);
+    }
+}
