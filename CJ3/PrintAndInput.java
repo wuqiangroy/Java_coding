5d1ef65a6074d69fd1ccd8fedad1f725cda774cb
wuqiangroy <wuqiangroy@live.com>
2017-11-09 23:45:09 +0800

    full of input and output

--- CJ3/PrintAndInput.java
+++ CJ3/PrintAndInput.java
@@ -1,8 +1,10 @@
+import java.io.PrintWriter;
 import java.util.*;
+import java.nio.file.Paths;
 
 
 public class PrintAndInput {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
 
@@ -11,5 +13,23 @@ public class PrintAndInput {
         System.out.println("How old are you?");
         int age = in.nextInt();
         System.out.println("Hello, "+name+".Next year, you'll be "+(age+1));
+        
+        // format print
+        double x = 1000.0 / 3.4;
+        System.out.printf("%8.2f", x);
+        System.out.printf("Hello, %s. Next year, you'll be %d", name, age+1);
+        System.out.printf("%,.2f", 1000.0/3.0); // 3,333.33 
+
+        String message = String.format("This is a float number %f", 1000.0/3.0);
+        System.out.println(message);
+
+        // format date
+        System.out.printf("%1$s %2$tB %2$te, %2$tY", "Due data: ", new Date());
+        // Due data:  November 9, 2017
+
+        // input file and output
+        // Scanner inFile = new Scanner(Paths.get("DataString.java"));
+        // out
+        // PrintWriter outFile = new PrintWriter("output.txt");
     }
 }
