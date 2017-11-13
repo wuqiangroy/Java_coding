74aaf9204f6950d17d56f959dc680d6ce710dbc2
wuqiangroy <wuqiangroy@live.com>
2017-11-13 23:54:26 +0800

    update

---
+++ CJ4/DefineClass/ConstructorTest.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+
+public class ConstructorTest {
+    public static void main(String[] args) {
+        Employee4[] staff = new Employee4[3];
+        staff[0] = new Employee4("Harry", 4000);
+        staff[1] = new Employee4(6000);
+        staff[2] = new Employee4();
+        
+        for (Employee4 e: staff) {
+            System.out.println("name: "+e.getName()+" id: "+e.getId()+" salary: "+e.getSalary());
+        }
+    }
+    
+}
+
+
+class Employee4 {
+    private static int nextId;
+
+    private int id;
+    private String name = ""; // instance field initialization
+    private double salary;
+
+    static {    
+        Random generator = new Random();
+        nextId = generator.nextInt(1000);
+    }
+
+    {
+        id = nextId;
+        nextId++;
+    }
+
+    public Employee4(String n, double s) {
+        name = n;
+        salary = s;
+    }
+
+    public Employee4(double s) {
+        this("Employee4 #" + nextId, s);
+    }
+
+    public Employee4() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
