63b45b7bd27f3a34f511d74e9c3a639fab094201
wuqiangroy <wuqiangroy@live.com>
2017-11-20 19:43:05 +0800

    inner class

---
+++ CJ6/innerClass/InnerClassTest.java
@@ -0,0 +1,42 @@
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class InnerClassTest {
+    public static void main(String[] args) {
+        TalkingClock clock = new TalkingClock(1000, true);
+        clock.start();
+
+        JOptionPane.showMessageDialog(null, "Quit programe");
+        System.exit(0);
+    }
+}
+
+
+class TalkingClock {
+    private int interval;
+    private boolean beep;
+
+    public TalkingClock(int interval, boolean beep) {
+        this.interval = interval;
+        this.beep = beep;
+    }
+
+    public void start() {
+        ActionListener listener = new TimePrinter();
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+
+    public class TimePrinter implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Date now = new Date();
+            System.out.println("At the tone, the time is " + now);
+            if (beep) {
+                java.awt.Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+}
